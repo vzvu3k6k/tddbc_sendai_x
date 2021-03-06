190f3c59a56a1864e214de8ed8eb200fb6068be9
vzvu3k6k <vzvu3k6k@gmail.com>
2021-03-06 11:03:51 +0900

    RED: 品種を3種類、サイズを4種類に限定する

--- spec/lib/strawberry_spec.rb
+++ spec/lib/strawberry_spec.rb
@@ -1,7 +1,16 @@
 RSpec.describe Strawberry do
   describe '#initialize' do
-    it do
-      expect(Strawberry.new('あまおう', 'M')).to be_a(Strawberry)
+    context '成功' do
+      it do
+        expect(Strawberry.new('あまおう', 'M')).to be_a(Strawberry)
+      end
+    end
+
+    context '失敗' do
+      it do
+        expect { Strawberry.new('無効な品種', '無効なサイズ') }.to raise_error ArgumentError
+      end
+      # なお、品種は、 あまおう とちおとめ もういっこ の3種類、いちごのサイズは S M L LL の4種類です。
     end
   end
 
